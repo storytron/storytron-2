e71c59e5ae421bf5de242fb25d7fabb297696a9a
Bill Maya <bill.maya@gmail.com>
2020-03-22 13:42:43 -0400

    Added scene editor controls.

--- src/scenes/ScenesController.java
+++ src/scenes/ScenesController.java
@@ -1,4 +1,23 @@
 package scenes;
 
-public class ScenesController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ScenesController implements Initializable {
+
+    final ObservableList<String> sceneListItems = FXCollections.observableArrayList("Scene 1", "Scene 2", "Scene 3", "Scene 4", "Scene 5");
+
+    @FXML
+    private ListView<String> sceneList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        sceneList.setItems(sceneListItems);
+    }
 }
