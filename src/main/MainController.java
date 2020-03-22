653858a94af934d321c003903246019a82b556e4
Bill Maya <bill.maya@gmail.com>
2020-03-22 13:41:46 -0400

    Removed unused MenuItem control. Modified About dialog contents.

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -2,8 +2,6 @@ package main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.stage.Window;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.Alert;
 
 
@@ -11,15 +9,12 @@ import javafx.scene.control.Alert;
 public class MainController {
 
     @FXML
-    private MenuItem menuItemAbout;
-
-    @FXML
     protected void displayAboutDialog(ActionEvent event) {
        //System.out.println("About menu clicked");
         Alert about = new Alert(Alert.AlertType.INFORMATION);
         about.setTitle("Storytron 2.0");
         about.setHeaderText(null);
-        about.setContentText("\"Omnes idea nulla superbia\"\n\nAll ideas, no pride");
+        about.setContentText("v2.0-alpha1\n\n\"Omnes idea nulla superbia\"\nAll ideas, no pride");
         about.showAndWait();
     }
 
