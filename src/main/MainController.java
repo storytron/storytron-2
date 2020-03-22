1ab3986020618d539d7063da95ed03cea4ce8c3c
Bill Maya <bill.maya@gmail.com>
2020-03-22 10:53:19 -0400

    Added fx:id and onAction to About menu item in main.fxml. Added corresponding MenuItem control and displayAboutDialog method to MainController.

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,4 +1,21 @@
 package main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Window;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert;
+
+
+
 public class MainController {
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    protected void displayAboutDialog(ActionEvent event) {
+
+    }
+
 }
