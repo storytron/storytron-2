22c7261b9d0ce43ce7dfa3f78b3b29e2f400f55d
Bill Maya <bill.maya@gmail.com>
2020-04-10 07:49:43 -0400

    Displayed scene icon.

--- src/scenes/ScenesController.java
+++ src/scenes/ScenesController.java
@@ -6,6 +6,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,8 +21,18 @@ public class ScenesController implements Initializable {
     @FXML
     private ListView<String> sceneList;
 
+    @FXML
+    private ImageView sceneIcon;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+        // Create image for scene editor icon
+        URL imageUrl = this.getClass().getResource("/resources/audience.png");
+        Image image = new Image(String.valueOf(imageUrl));
+
+        sceneIcon.setImage(image);
         sceneList.setItems(sceneListItems);
     }
 }
